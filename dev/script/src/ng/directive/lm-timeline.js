3c5ce0ec21f6fad5fb9fec87ab5bed5e7af5057f
dudewad <layton@desmill.com>
2016-01-04 00:07:00 -0800

    Timeline supports mobile swipe, mouse swipe, and mouse wheel navigation. Still needs touch-click option.

--- dev/script/src/ng/directive/lm-timeline.js
+++ dev/script/src/ng/directive/lm-timeline.js
@@ -11,6 +11,12 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 			var _pageTransitionComplete = false;
 			var _timelineBuilt = false;
 			var _acceleration = 50;
+			var _defaultTransitionTime = 2;
+			var _mouseWheelTransition = false;
+			var _direction = {
+				next: "next",
+				prev: "prev"
+			};
 
 
 
@@ -25,13 +31,18 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 
 
 
-			function _enter() {
+			function _enter(dir) {
+				dir = dir ? dir : _direction.next;
 				_killSwipeTween();
 				var target = element.find(".timeline-event").eq(scope.timeline.currentEvent);
 				var center = angular.element(window).outerHeight() / 2 - target.outerHeight() / 2;
+				var topPos = dir === _direction.next ? "100%" : "-" + target.outerHeight();
 
 				_swipeTargetTween = new TimelineMax();
-				_swipeTargetTween.to(target, 2.5, {top: center, ease: Power4.easeOut});
+				_swipeTargetTween   .set(target, {top: topPos})
+									.to(target, _defaultTransitionTime, {top: center, ease: Power4.easeOut});
+
+				_prevEvent = scope.timeline.currentEvent;
 			}
 
 
@@ -41,7 +52,7 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 				if(scope.timeline.currentEvent === scope.timeline.events.length){
 					scope.timeline.currentEvent = 0;
 				}
-				_enter();
+				_enter(_direction.next);
 			}
 
 
@@ -51,7 +62,7 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 				if (scope.timeline.currentEvent < 0) {
 					scope.timeline.currentEvent = scope.timeline.events.length - 1;
 				}
-				_enter();
+				_enter(_direction.prev);
 			}
 
 
@@ -98,6 +109,14 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 
 
 
+			function _updateSwipeTarget(){
+				_swipeTarget.css({
+					top: _positioning.top + "px"
+				});
+			}
+
+
+
 			scope.$on("interactionEnd", function (e, pointerData, originalEvent) {
 				_positioning = {};
 				var speed = pointerData.ySpeed * 250;
@@ -127,6 +146,29 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 
 
 
+			function _handleMouseWheel(e){
+				if(_mouseWheelTransition){
+					return;
+				}
+				var target = element.find(".timeline-event").eq(scope.timeline.currentEvent);
+				var posTop = e.originalEvent.deltaY > 0 ? "100%" : "-" + target.outerHeight() + "px";
+				var t = new TimelineMax({
+					onComplete: function(){
+						_mouseWheelTransition = false;
+					}
+				});
+				t.to(target, _defaultTransitionTime / 2, {top: posTop});
+				if(e.originalEvent.deltaY > 0){
+					_previous();
+				}
+				else{
+					_next();
+				}
+				_mouseWheelTransition = true;
+			}
+
+
+
 			scope.$on("interactionSwipe", function (e, pointerData, originalEvent) {
 				if(!_hasClickCancel){
 					var anchor = _swipeTarget.find("a");
@@ -138,16 +180,14 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 				_positioning = {
 					top: _positioning.top + pointerData.yDif
 				};
-
-				_swipeTarget.css({
-					top: _positioning.top + "px"
-				});
+				_updateSwipeTarget();
 				_applyScope();
 			});
 
 
 
 			scope.$on("interactionStart", function (e, pointerData, originalEvent) {
+				_killSwipeTween();
 				_swipeTarget = $(originalEvent.target).closest(".timeline-event");
 				_positioning = {
 					top: parseInt(_swipeTarget.css("top"))
@@ -158,6 +198,7 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 
 			scope.$on("$destroy", function () {
 				GlobalEventsService.unregisterResizeHandler(_resizeHandlerID);
+				angular.element("body").off("mousewheel", _handleMouseWheel);
 			});
 
 
@@ -173,6 +214,10 @@ angular.module("LMApp").directive("lmTimeline", ["CONSTANT", "GlobalEventsServic
 				_pageTransitionComplete = true;
 				_init();
 			});
+
+
+
+			angular.element("body").on("mousewheel", _handleMouseWheel);
 		}
 	};
 }]);
